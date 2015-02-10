eee3a2427e415cc1dcff1d1c9de7c62fc72b7cdc
Uduse <wzy950618@gmail.com>
2015-02-09 16:39:36 -0800

    Better Readme

--- Linux/Escape_Sequences_Colors.h
+++ Linux/Escape_Sequences_Colors.h
@@ -1,41 +1,50 @@
 /*!
- * File:
- * Escape Sequence Color Header.h
- *
- * Date:
- * 2015/02/09 16:13
- *
- * Author:
- * Uduse
- *
- *
- *
- * Text Attribute Options:
- * ATTRIBUTES_OFF, BOLD, UNDERSCORE, BLINK, REVERSE_VIDEO, CONCEALED
- *
- * Color Options:
- * BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE
- * 
- * 
- * Format:
- * COLOR_$Foreground Color$_$Background Color$
- * COLOR_$Text Attribute$_$Foreground Color$_$Background Color$
- * COLOR_NORMAL // To set color to default
- * e.g.
- * COLOR_BLUE_BLACK // Leave Text Attribute Blank if no Text Attribute appied
- * COLOR_UNDERSCORE_YELLOW_RED
- * COLOR_NORMAL
- *
- *
- * Usage:
- *
- * Just use  to stream the color you want before outputing text and
- * use  again to set the color to normal after outputing text.
- * e.g. cout << COLOR_BLUE_BLACK << "TEXT" << COLOR_NORMAL << endl;
- * e.g. cout << COLOR_BOLD_YELLOW_CYAN << "TEXT" << COLOR_NORMAL << endl;
- *
- *
- */
+* # Escape-Sequence-Color-Header
+* Color Your Output in Unix!!
+* 
+* 
+* ##File:
+* Escape Sequence Color Header.h
+* 
+* ##Date:
+* 2015/02/09 16:13 (~3h)
+* 
+* ##Author:
+* Uduse
+* 
+* 
+* 
+* ##Text Attribute Options:
+* ATTRIBUTES_OFF, BOLD, UNDERSCORE, BLINK, REVERSE_VIDEO, CONCEALED
+* 
+* ##Color Options:
+* BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE
+* 
+* 
+* ##Format:
+* 
+* General Format, include value you want in $variable$
+* 
+* COLOR_$Foreground Color$_$Background Color$
+* COLOR_$Text Attribute$_$Foreground Color$_$Background Color$
+* COLOR_NORMAL  // To set color to default
+* 
+* 
+* e.g.
+* 
+* COLOR_BLUE_BLACK // Leave Text Attribute Blank if no Text Attribute appied
+* COLOR_UNDERSCORE_YELLOW_RED
+* COLOR_NORMAL
+* 
+* 
+* ##Usage:
+* 
+* Just use  to stream the color you want before outputting text and
+* use  again to set the color to normal after outputting text.
+* 
+* cout << COLOR_BLUE_BLACK << "TEXT" << COLOR_NORMAL << endl;
+* cout << COLOR_BOLD_YELLOW_CYAN << "TEXT" << COLOR_NORMAL << endl;
+* */
 const char COLOR_NORMAL[] = { 0x1b, '[', '0', ';', '3', '9', 'm', 0 };
 const char COLOR_BLACK_BLACK[] = { 0x1b, '[', '0', ';', '3', '0', ';', '4', '0', 'm', 0 };
 const char COLOR_BLACK_RED[] = { 0x1b, '[', '0', ';', '3', '0', ';', '4', '1', 'm', 0 };
--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # Escape-Sequence-Color-Header
-Color Your Output!!
+Color Your Output in Unix!!
+
+
+##File:
+Escape Sequence Color Header.h
+ 
+##Date:
+2015/02/09 16:13 (~3h)
+ 
+##Author:
+Uduse
+ 
+ 
+ 
+##Text Attribute Options:
+	ATTRIBUTES_OFF, BOLD, UNDERSCORE, BLINK, REVERSE_VIDEO, CONCEALED
+ 
+##Color Options:
+	BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE
+ 
+ 
+##Format:
+
+General Format, include value you want in $variable$
+
+	COLOR_$Foreground Color$_$Background Color$
+	COLOR_$Text Attribute$_$Foreground Color$_$Background Color$
+	COLOR_NORMAL  // To set color to default
+
+
+e.g.
+
+	COLOR_BLUE_BLACK // Leave Text Attribute Blank if no Text Attribute appied
+	COLOR_UNDERSCORE_YELLOW_RED
+	COLOR_NORMAL
+ 
+ 
+##Usage:
+ 
+Just use  to stream the color you want before outputting text and
+use  again to set the color to normal after outputting text.
+
+	cout << COLOR_BLUE_BLACK << "TEXT" << COLOR_NORMAL << endl;
+	cout << COLOR_BOLD_YELLOW_CYAN << "TEXT" << COLOR_NORMAL << endl;
