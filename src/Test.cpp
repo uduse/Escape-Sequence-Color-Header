49f58790f175059c7d7228b863281bfaf2346974
Uduse <wzy950618@gmail.com>
2015-02-21 22:52:56 -0800

    ^^img

--- src/Test.cpp
+++ src/Test.cpp
@@ -36,16 +36,16 @@ int main( int argc, char* argv[] )
 	cout << endl;
 
 
-	// Tag
-	cout << endl;
-	cout << endl;
-	cout << endl;
-	cout << COLOR_RED_CYAN_COLOREND << "COLOR_RED_CYAN";
-	cout << COLOR_WHITE_MAGENTA_COLOREND << "COLOR_WHITE_MAGENTA";
-	cout << COLOR_CYAN_YELLOW_COLOREND << "COLOR_CYAN_YELLOW";
-	cout << COLOR_NORMAL_COLOREND;
-	cout << endl;
-	cout << endl;
-	cout << endl;
+	//// Tag
+	//cout << endl;
+	//cout << endl;
+	//cout << endl;
+	//cout << COLOR_RED_CYAN_COLOREND << "COLOR_RED_CYAN";
+	//cout << COLOR_WHITE_MAGENTA_COLOREND << "COLOR_WHITE_MAGENTA";
+	//cout << COLOR_CYAN_YELLOW_COLOREND << "COLOR_CYAN_YELLOW";
+	//cout << COLOR_NORMAL_COLOREND;
+	//cout << endl;
+	//cout << endl;
+	//cout << endl;
 
 }
